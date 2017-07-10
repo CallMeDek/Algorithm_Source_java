4bc2a58d5bdec822da9c41e9f0e95bf768992d4a
CallMeDek <choidaegyu9671@gmail.com>
2017-07-11 00:10:06 +0900

    Add maze algorithms

---
+++ com/inflearn/Maze_test.java
@@ -0,0 +1,19 @@
+package com.inflearn;
+
+import com.inflearn.Maze;
+
+public class Maze_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		Maze.getLength();
+		while(i < 20)
+		{
+			Maze.test();
+			System.out.println();
+			i++;
+		}
+	}
+
+}
